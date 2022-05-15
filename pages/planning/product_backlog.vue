6ce41cb61e3624a794d0b85d53b81d6bc96829fe
s-arikawa <arikawa@i3-systems.com>
2022-05-16 00:09:09 +0900

    tailwind/form を追加

--- pages/planning/product_backlog.vue
+++ pages/planning/product_backlog.vue
@@ -6,65 +6,61 @@ const backlog = ref([
   {id: "#2", title: "画面 A の実装", sp: 20},
   {id: "#3", title: "画面 A のテスト設計", sp: 12}
 ])
-
 const totalSP = computed(() => backlog.value.reduce((sum, task) => sum + task.sp, 0))
 const taskCount = computed(() => backlog.value.length)
+
+const addTask = () => {
+
+}
 </script>
 <template>
   <div>
-    <table>
-      <thead>
+    <div class="flex items-center gap-3">
+      <label class="block">
+        <span class="text-gray-700">ID:</span>
+        <input type="number" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+      </label>
+
+      <label>
+        <span>Title:</span>
+        <input type="text">
+      </label>
+
+      <div>
+        <label>
+          SP:
+          <input type="number">
+        </label>
+      </div>
+      <div>
+        <button type="button" @click="addTask">Enter</button>
+      </div>
+    </div>
+
+    <table class="table-auto border-collapse">
+      <thead class="border-b-2 border-b-gray-400">
       <tr>
-        <th class="w-16">ID</th>
-        <th class="w-96">Title</th>
-        <th class="w-40">Story Point</th>
+        <th class="px-3 py-5 w-16">ID</th>
+        <th class="px-3 py-5 w-96">Title</th>
+        <th class="px-3 py-5 w-40">Story Point</th>
       </tr>
       </thead>
       <tbody>
       <tr v-for="task in backlog">
-        <td class="ID">{{ task.id }}</td>
-        <td>{{ task.title }}</td>
-        <td class="Number">{{ task.sp }}</td>
+        <td class="p-3 text-center">{{ task.id }}</td>
+        <td class="p-3">{{ task.title }}</td>
+        <td class="p-3 text-right pr-10">{{ task.sp }}</td>
       </tr>
       </tbody>
-      <tfoot>
+      <tfoot class="border-t-2 border-t-gray-400">
       <tr>
-        <td>合計</td>
-        <td>{{ taskCount }} 課題</td>
-        <td class="Number">{{ totalSP }} SP</td>
+        <td class="p-3">合計</td>
+        <td class="p-3">{{ taskCount }} 課題</td>
+        <td class="p-3 text-right pr-10">{{ totalSP }} SP</td>
       </tr>
       </tfoot>
     </table>
   </div>
 </template>
 
-<style scoped>
-table {
-  @apply table-auto border-collapse;
-}
-
-thead {
-  @apply border-b-2 border-b-gray-400;
-}
-
-th {
-  @apply px-3 py-5;
-}
-
-td {
-  @apply p-3;
-}
-
-tfoot {
-  @apply border-t-2 border-t-gray-400;
-}
-
-.ID {
-  @apply text-center;
-}
-
-.Number {
-  @apply text-right pr-10;
-}
-
-</style>
+<style scoped></style>
