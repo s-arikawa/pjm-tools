132b301fee155227d20a3b7ba47c677e90209f3f
s-arikawa <arikawa@i3-systems.com>
2022-06-03 17:06:48 +0900

    ヘッダー作成中　ロゴ追加

--- components/MHeader.vue
+++ components/MHeader.vue
@@ -1,10 +1,17 @@
 <script lang="ts" setup></script>
 
 <template>
-  <header class="flex items-center bg-emerald-500 p-4">
-    <ServiceLogo class="ml-3 mr-5"/>
+  <header class="flex items-center h-16 bg-white Header">
+    <ServiceLogo class="m-4"/>
     <NuxtLink to="/">
-      <h1 class="text-xl text-white">俺的最強のプロジェクト管理ツール</h1>
+      <h1 class="text-xl">俺的最強のプロジェクト管理ツール</h1>
     </NuxtLink>
   </header>
 </template>
+
+<style scoped>
+.Header {
+  box-shadow: 0px 5px 12px rgba(0, 0, 0, 0.08);
+}
+
+</style>
